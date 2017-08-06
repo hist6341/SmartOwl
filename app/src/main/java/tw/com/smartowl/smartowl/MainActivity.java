ca13b43d38382fffd4793a6322f76e713ec8437d
MING <hist6341@gmail.com>
2017-08-06 20:09:03 +0800

    Navigation_part1

--- app/src/main/java/tw/com/smartowl/smartowl/MainActivity.java
+++ app/src/main/java/tw/com/smartowl/smartowl/MainActivity.java
@@ -1,13 +1,26 @@
 package tw.com.smartowl.smartowl;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.SearchManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -16,6 +29,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -25,10 +39,15 @@ public class MainActivity extends AppCompatActivity {
     ArrayAdapter<String> adapter;
     DatabaseReference reference_contacts = FirebaseDatabase.getInstance().getReference("Products");
 
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ///////////////////////////////////////////////////////////////////////////
 
         adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1,
@@ -56,6 +75,9 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+    ////////////////////////////////////////////////////////////////////////////////////////
+
+
     public void go_createProduct(View v) {
         startActivity(new Intent(this,CreateProduct.class));
     }
@@ -78,10 +100,10 @@ public class MainActivity extends AppCompatActivity {
             list.clear();
             Log.i("Adapter: ","Clear");
             for (DataSnapshot ds : dataSnapshot.getChildren() ){
-
-                adapter.add(ds.child("name").getValue().toString());
-
                 Product product = ds.getValue(Product.class);
+                adapter.add(ds.child("name").getValue().toString());
+                Log.i("Key",ds.child("name").getValue().toString());
+
                 list.add(product);
             }
         }
--- app/src/main/java/tw/com/smartowl/smartowl/ProductActivity.java
+++ app/src/main/java/tw/com/smartowl/smartowl/ProductActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -48,6 +49,10 @@ public class ProductActivity extends AppCompatActivity {
 
 
 
+    }
+
+    public void go_wikitude(View v) {
+        startActivity(new Intent(this,WikitudeActivity.class));
     }
     @Override
     public void onStart(){
